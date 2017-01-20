6f21ae8d27a1889de045519c9dafc4cb48c18c5e
video_error <viderror@gmail.com>
2017-01-19 18:24:39 -0800

    Optimized, reformatted, and re-commented GenerateNewLevelScreen.cs.

--- Survival/UI/GenerateNewLevelScreen.cs
+++ Survival/UI/GenerateNewLevelScreen.cs
@@ -26,102 +26,128 @@ using OpenTK.Input;
 namespace ClassicalSharp.Survival.UI {
 
 	/// <summary>
-	/// A class responsible for managing all the elements of the generate new level screen.
+	/// Holds all the elements for initializing, handling, and rendering the generate new level screen.
 	/// </summary>
 	internal sealed class GenerateNewLevelScreen : MenuOptionsScreen {
 
-		// Declares the wrapper value used for grabbing all elements used by the survival test mod.
+		// Declares the wrapper value.
 		private readonly Wrapper wrapper;
 
 		public GenerateNewLevelScreen(Game game) : base(game) {
-			// Assigns the wrapper value by creating a new instance and inputing the needed game element.
+			// Assigns the wrapper value to a new wrapper by using the needed game value.
 			wrapper = new Wrapper(game);
 		}
 
 		/// <summary>
-		/// Responsible for initializing the generate new level screen.
+		/// Responsible for initializing the generate new level screen's elements.
 		/// </summary>
 		public override void Init() {
-			// Initializes the base extended and inherited class.
+			// Initializes the base extended and inherited class's elements.
+			// This is required since it was overrided with this method.
 			base.Init();
 
-			// Creates a new instance of text widget for the generate new level text using the game value
-			// and the regular font value.
+			// Declares and assigns the generate new level text to a new chat text widget by using the needed game value
+			// and regular font value.
 			ChatTextWidget GenerateNewLevelText = new ChatTextWidget(game, regularFont);
 
 			// Initializes the generate level text value.
 			GenerateNewLevelText.Init();
 
-			// Sets the text for the generate level text value
+			// Sets the text of the game over text's value by using the needed text as "Generate new level."
 			GenerateNewLevelText.SetText("Generate new level");
 
-			// Initializes all the widgets through an array.
+			// Assigns the widgets array value to a new widgets array.
 			widgets = new Widget[] {
-				// Adds the new level text to the widgets array.
+				// Adds the generate new level text.
 				GenerateNewLevelText,
 
-				// Creates and adds the small level generation title button widget.
+				// Adds the small title by using the needed direction value as zero,
+				// the Y value as negative one-hundred and twenty-five, the text as "Small", and the small level handler.
 				MakeTitle(0, -125, "Small", SmallLevelHandler),
-				// Creates and adds the large level generation title button widget.
+				// Adds the large title by using the needed direction value as zero,
+				// the Y value as negative seventy-five, the text as "Large", and the large level handler.
 				MakeTitle(0, -75, "Large", LargeLevelHandler),
-				// Creates and adds the huge level generation title button widget.
+				// Adds the huge title by using the needed direction value as zero,
+				// the Y value as negative twenty-five, the text as "Huge", and the huge level handler.
 				MakeTitle(0, -25, "Huge", HugeLevelHandler),
-				// Creates and adds the cancel title button widget.
+				// Adds the canel title by using the needed direction value as zero,
+				// the Y value as one-hundred and twenty-five, the text as "Cancel", and the cancel handler.
 				MakeTitle(0, 125, "Cancel", CancelHandler),
 
-				null, null
+				// Adds the last two values as null.
+				null,
+				null
 			};
 		}
 
 		/// <summary>
-		/// Responsible for handling the small level generation title button widget.
+		/// Responsible for handling the small level generation title button element.
 		/// </summary>
 		private void SmallLevelHandler(Game game, Widget widget, MouseButton mouseButton) {
+			// Detects whether the mouse button equals the left mouse button and whether the mouse is pressed.
 			if(mouseButton == MouseButton.Left && game.IsMousePressed(mouseButton)) {
+				// Generates a new map by using the needed width value as one-hundred and twenty-eight,
+				// the hieght value as sixty-four, the length value as one-hundred and twenty-eight,
+				// the new random integer, and the new notchy generator.
 				wrapper.GetSinglePlayerServer.GenMap(128, 64, 128, new Random().Next(), new NotchyGenerator());
 			}
 		}
 
 		/// <summary>
-		/// Responsible for handling the large level generation title button widget.
+		/// Responsible for handling the large level generation title button element.
 		/// </summary>
 		private void LargeLevelHandler(Game game, Widget widget, MouseButton mouseButton) {
+			// Detects whether the mouse button equals the left mouse button and whether the mouse is pressed.
 			if(mouseButton == MouseButton.Left && game.IsMousePressed(mouseButton)) {
+				// Generates a new map by using the needed width value as two-hundred and fifty-six,
+				// the hieght value as sixty-four, the length value as two-hundred and fifty-six,
+				// the new random integer, and the new notchy generator.
 				wrapper.GetSinglePlayerServer.GenMap(256, 64, 256, new Random().Next(), new NotchyGenerator());
 			}
 		}
 
 		/// <summary>
-		/// Responsible for handling the huge level generation title button widget.
+		/// Responsible for handling the huge level generation title button element.
 		/// </summary>
 		private void HugeLevelHandler(Game game, Widget widget, MouseButton mouseButton) {
+			// Detects whether the mouse button equals the left mouse button and whether the mouse is pressed.
 			if(mouseButton == MouseButton.Left && game.IsMousePressed(mouseButton)) {
+				// Generates a new map by using the needed width value as five-hundred and twelve,
+				// the hieght value as sixty-four, the length value as five-hundred and twelve,
+				// the new random integer, and the new notchy generator.
 				wrapper.GetSinglePlayerServer.GenMap(512, 64, 512, new Random().Next(), new NotchyGenerator());
 			}
 		}
 
 		/// <summary>
-		/// Responsible for handling the cancel title button widget.
+		/// Responsible for handling the cancel title button element.
 		/// </summary>
 		private void CancelHandler(Game game, Widget widget, MouseButton mouseButton) {
+			// Detects whether the mouse button equals the left mouse button and whether the mouse is pressed.
 			if(mouseButton == MouseButton.Left && game.IsMousePressed(mouseButton)) {
+				// Sets the foremost screen to the death screen.
 				wrapper.GetIGuiInterface.SetNewScreen(new DeathScreen(game));
 			}
 		}
 
 		/// <summary>
-		/// Responsible for rendering the elements of the new level generation screen.
+		/// Responsible for rendering all the new level generation screen elements.
 		/// </summary>
 		public override void Render(double delta) {
+			// Moves the game over text by using the needed X and Y value.
 			widgets[0].MoveTo(game.Width / 2 - widgets[0].Width / 2, (widgets[1].Y - widgets[0].Height) - widgets[1].Height / 2);
 
+			// Renders the base extended and inherited class's elements.
+			// This is required since it was overrided with this method.
 			base.Render(delta);
 		}
 
 		/// <summary>
-		/// Responsible for handling what happens when a key is down.
+		/// Responsible for handling when a key is down.
 		/// </summary>
 		public override bool HandlesKeyDown(Key key) {
+			// Disables the ESC key by using the needed GUI interface,
+			// the key parameter, this instance, and a new instance of the generate new level screen with the needed game value.
 			return Utilities.DisableEscapeKey(wrapper.GetIGuiInterface, key, this, new GenerateNewLevelScreen(game));
 		}
 	}
